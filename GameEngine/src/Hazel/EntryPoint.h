c70c1283a54d21a619988d0329a984ff61433ca6
Kinsxi <kinsxi2022@outlook.com>
2024-08-18 00:38:26 +0800

    Log & Using Libraries

--- GameEngine/src/Hazel/EntryPoint.h
+++ GameEngine/src/Hazel/EntryPoint.h
@@ -7,7 +7,12 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hello World!\n");
+
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Intialized log.");
+	int a = 4;
+	HZ_INFO("Hello! test_v = {0}",a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
